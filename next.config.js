c934d8e278d35e2cc23d6efcfd81516fee294d8c
computerkingminsu <ya5656@naver.com>
2024-02-28 00:21:49 +0900

    fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   trailingSlash: true,
+//   swcMinify: true,
+//   experimental: {
+//     emotion: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+// module.exports = {
+//   images: {
+//     domains: ['i.ibb.co', 'firebasestorage.googleapis.com'],
+//   },
+// };
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,12 +22,9 @@ const nextConfig = {
   experimental: {
     emotion: true,
   },
+  images: {
+    domains: ['i.ibb.co', 'firebasestorage.googleapis.com'],
+  },
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  images: {
-    domains: ['i.ibb.co'],
-  },
-};
